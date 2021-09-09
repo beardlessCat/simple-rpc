71143da39a701f7d308337dcfaccfcda184e800c
lyj <lyj@dareway.com.cn>
2021-09-09 19:04:22 +0800

    add：增加编码器与解码器

---
+++ common/src/main/java/com/common/codedc/JSONDecoder.java
@@ -0,0 +1,28 @@
+package com.common.codedc;
+
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+/**
+ * 解码器
+ */
+public class JSONDecoder extends LengthFieldBasedFrameDecoder {
+
+    public JSONDecoder() {
+        super(65535, 0, 4,0,4);
+    }
+
+    protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        ByteBuf decode = (ByteBuf) super.decode(ctx, in);
+        if (decode==null){
+            return null;
+        }
+        int data_len = decode.readableBytes();
+        byte[] bytes = new byte[data_len];
+        decode.readBytes(bytes);
+        Object parse = JSON.parse(bytes);
+        return parse;
+    }
+}

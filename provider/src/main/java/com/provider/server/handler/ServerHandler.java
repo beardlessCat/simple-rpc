7e20882ea217eda96551d6dfa54c57f79c5583b2
lyj <lyj@dareway.com.cn>
2021-09-13 19:50:05 +0800

    mod：调整请求处理逻辑

--- provider/src/main/java/com/provider/server/handler/ServerHandler.java
+++ provider/src/main/java/com/provider/server/handler/ServerHandler.java
@@ -1,11 +1,15 @@
 package com.provider.server.handler;
 
+import com.alibaba.fastjson.JSON;
+import com.common.entity.RpcRequest;
+import com.common.entity.RpcResponse;
 import com.provider.holder.ServiceHolder;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 @ChannelHandler.Sharable
@@ -27,7 +31,44 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //处理远程连接，执行本地方法
+        RpcRequest request = JSON.parseObject(msg.toString(),RpcRequest.class);
+        Object result = this.handleRequest(request);
+        System.out.printf("", result);
+    }
+
+    private Object handleRequest(RpcRequest request) throws Exception {
+        String className = request.getClassName();
         Map<String, Object> serviceMap = ServiceHolder.serviceMap;
-        super.channelRead(ctx, msg);
+        Object serviceBean = serviceMap.get(className);
+        if (serviceBean!=null){
+            Class<?> serviceClass = serviceBean.getClass();
+            String methodName = request.getMethodName();
+            Class<?>[] parameterTypes = request.getParameterTypes();
+            Object[] parameters = request.getParameters();
+            Method method = serviceClass.getMethod(methodName, parameterTypes);
+            method.setAccessible(true);
+            Object invoke = method.invoke(serviceBean, getParameters(parameterTypes, parameters));
+            return invoke ;
+        }else{
+            throw new Exception("未找到服务接口");
+        }
+    }
+    /**
+     * 获取参数列表
+     * @param parameterTypes
+     * @param parameters
+     * @return
+     */
+    private Object[] getParameters(Class<?>[] parameterTypes,Object[] parameters){
+        if (parameters==null || parameters.length==0){
+            return parameters;
+        }else{
+            Object[] new_parameters = new Object[parameters.length];
+            for(int i=0;i<parameters.length;i++){
+                new_parameters[i] = JSON.parseObject(parameters[i].toString(),parameterTypes[i]);
+            }
+            return new_parameters;
+        }
     }
 }

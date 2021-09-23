f02554b90adca1f9af0a1b69a8dbc0478f564ec5
lyj <lyj@dareway.com.cn>
2021-09-23 22:02:11 +0800

    add：支持不同的序列化/反序列化

---
+++ common/src/main/java/com/serializer/hessian/HessianSerializer.java
@@ -0,0 +1,62 @@
+package com.serializer.hessian;
+
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
+import com.serializer.Serializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class HessianSerializer extends Serializer {
+
+    @Override
+    public <T> byte[] serialize(T obj) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        Hessian2Output ho = new Hessian2Output(os);
+        try {
+            ho.writeObject(obj);
+            ho.flush();
+            byte[] result = os.toByteArray();
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                ho.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                os.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    @Override
+    public <T> Object deserialize(byte[] bytes, Class<T> clazz) {
+        ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+        Hessian2Input hi = new Hessian2Input(is);
+        try {
+            Object result = hi.readObject();
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                hi.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                is.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
